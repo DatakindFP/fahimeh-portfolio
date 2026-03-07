4d867713cb58b9150e597a60ee758fce548a452b
Fahimeh Pouryani <pouryanf@mskcc.org>
2026-03-07 18:50:07 -0500

    Change the page base

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/DatakindFP/',  // ← CHANGE THIS to your actual repo name
+  base: '/fahimeh-portfolio/',  // 
 })
